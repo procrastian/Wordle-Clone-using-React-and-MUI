526170a0c33b631030b496c968dc0a5283bc64ca
Your Name <your_email@address>
2023-10-04 17:23:01 +0100

    guess letter state started

--- wordle_clone/src/components/Guesses.js
+++ wordle_clone/src/components/Guesses.js
@@ -1,9 +1,19 @@
 import Letter from "./Letter";
+import { useState } from "react";
 export default function Guesses() {
+  const initialState = ["", "", "", "", ""];
+  const [guessState, setGuessState] = useState(initialState);
+
+  console.log(guessState);
   return (
     <section className="guesses">
-      <h2>Guesses</h2>
-      <Letter />
+      <ul>
+        {guessState.map((item, index) => (
+          <li className="letter-card" key={index}>
+            <Letter />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
