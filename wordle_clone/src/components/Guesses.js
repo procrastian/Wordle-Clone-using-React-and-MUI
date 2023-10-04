755d14808abeb1cec26544d105b954de04499f7f
Your Name <your_email@address>
2023-10-04 14:03:59 +0100

    css section classes set up

--- wordle_clone/src/components/Guesses.js
+++ wordle_clone/src/components/Guesses.js
@@ -1,7 +1,7 @@
 export default function Guesses() {
   return (
-    <>
+    <section className='guesses'>
       <h2>Guesses</h2>
-    </>
+    </section>
   );
 }
--- wordle_clone/src/components/Keyboard.js
+++ wordle_clone/src/components/Keyboard.js
@@ -1,7 +1,7 @@
 export default function Keyboard() {
   return (
-    <>
+    <section className='keyboard'>
       <h2>Keyboard</h2>
-    </>
+    </section>
   );
 }
