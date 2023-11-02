5eaf437c6e9845fbf54e2719550eda92fa48f5e7
Your Name <your_email@address>
2023-11-02 14:42:34 +0000

    keyboard not using dom to render

--- wordle_clone/src/Components/App/App.js
+++ wordle_clone/src/Components/App/App.js
@@ -1,15 +1,12 @@
-import SimpleContainer from "../Container/Container.js";
 import BasicGrid from "../Guess/GuessGrid.js";
-import KeyboardApp from "../Keyboard/KeyboardApp.js";
+import KeyboardDisplay from "../Keyboard/KeyboardApp.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <BasicGrid />
-      <SimpleContainer>
-        <KeyboardApp />
-      </SimpleContainer>
+      <KeyboardDisplay id="keyboard" />
     </div>
   );
 }
--- wordle_clone/src/Components/Keyboard/KeyboardApp.js
+++ wordle_clone/src/Components/Keyboard/KeyboardApp.js
@@ -1,74 +1,46 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, { useRef, useState } from "react";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
-export default function KeyboardApp() {
-  class App extends Component {
-    state = {
-      layoutName: "default",
-      input: "",
-    };
+export default function KeyboardDisplay() {
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const keyboard = useRef();
 
-    onChange = (input) => {
-      this.setState({
-        input: input,
-      });
-      console.log("Input changed", input);
-    };
+  const onChange = (input) => {
+    setInput(input);
+    console.log("Input changed", input);
+  };
 
-    onKeyPress = (button) => {
-      console.log("Button pressed", button);
-    };
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
 
-    onChangeInput = (event) => {
-      let input = event.target.value;
-      this.setState(
-        {
-          input: input,
-        },
-        () => {
-          this.keyboard.setInput(input);
-        }
-      );
-    };
+  const onKeyPress = (button) => {
+    console.log("Button pressed", button);
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
 
-    render() {
-      return (
-        <div>
-          <input
-            value={this.state.input}
-            placeholder={"Tap on the virtual keyboard to start"}
-            onChange={(e) => this.onChangeInput(e)}
-          />
-          <Keyboard
-            keyboardRef={(r) => (this.keyboard = r)}
-            onChange={(input) => this.onChange(input)}
-            onKeyPress={(button) => this.onKeyPress(button)}
-            theme={"hg-theme-default hg-layout-default myTheme"}
-            layoutName={this.state.layoutName}
-            layout={{
-              default: [
-                "Q W E R T Y U I O P",
-                "A S D F G H J K L",
-                "{enter} Z X C V B N M {bksp}",
-              ],
-            }}
-            buttonTheme={[
-              {
-                class: "hg-red",
-                buttons: "Q W E R T Y q w e r t y",
-              },
-              {
-                class: "hg-highlight",
-                buttons: "Q q",
-              },
-            ]}
-          />
-        </div>
-      );
-    }
-  }
+  const onChangeInput = (event) => {
+    const input = event.target.value;
+    setInput(input);
+    keyboard.current.setInput(input);
+  };
 
-  render(<App />, document.getElementById("root"));
+  return (
+    <div className="App">
+      <input
+        value={input}
+        placeholder={"Tap on the virtual keyboard to start"}
+        onChange={onChangeInput}
+      />
+      <Keyboard
+        keyboardRef={(r) => (keyboard.current = r)}
+        layoutName={layout}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+    </div>
+  );
 }
