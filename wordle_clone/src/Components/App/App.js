6ce0edb3970d5d3d8ef85a4647afcfb7bdff85cf
Your Name <your_email@address>
2023-11-02 12:42:54 +0000

    grid for guesses and container for keyboard added

--- wordle_clone/src/Components/App/App.js
+++ wordle_clone/src/Components/App/App.js
@@ -1,10 +1,15 @@
+import SimpleContainer from "../Container/Container.js";
+import BasicGrid from "../Guess/GuessGrid.js";
 import KeyboardApp from "../Keyboard/KeyboardApp.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <KeyboardApp />
+      <BasicGrid />
+      <SimpleContainer>
+        <KeyboardApp />
+      </SimpleContainer>
     </div>
   );
 }
