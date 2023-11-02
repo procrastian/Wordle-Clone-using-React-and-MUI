6624177a5d60c560aa77ab3b128f3a0fb2286b3c
Your Name <your_email@address>
2023-11-02 14:59:56 +0000

    cards for 5 letters of single guess created

--- wordle_clone/src/Components/App/App.js
+++ wordle_clone/src/Components/App/App.js
@@ -1,11 +1,11 @@
-import BasicGrid from "../Guess/GuessGrid.js";
+import GuessGrid from "../Guess/GuessGrid.js";
 import KeyboardDisplay from "../Keyboard/KeyboardApp.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <BasicGrid />
+      <GuessGrid />
       <KeyboardDisplay />
     </div>
   );
--- wordle_clone/src/Components/Guess/GuessGrid.js
+++ wordle_clone/src/Components/Guess/GuessGrid.js
@@ -1,34 +1,27 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+export default function GuessGrid() {
 
-export default function BasicGrid() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
+    <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" spacing={2}>
+          {[0, 1, 2, 3, 4].map((value) => (
+            <Grid key={value} item>
+              <Paper
+                sx={{
+                  height: 140,
+                  width: 100,
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                }}
+              />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
-    </Box>
+    </Grid>
   );
 }
