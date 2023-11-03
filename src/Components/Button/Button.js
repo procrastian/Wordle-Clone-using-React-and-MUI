790e2686a78dbcd86373b1f56ee2c13480ab6737
Your Name <your_email@address>
2023-11-03 15:47:21 +0000

    reset button will save new word to app level target word state

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function ResetButton({setTargetWord}) {
 
+  const [newWord, setNewWord] = useState('')
+
   useEffect(() => {
     fetch('https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase')
       .then(response => response.json())
-      .then(json => setTargetWord(json))
+      .then(json => setNewWord(json))
       .catch(error => console.error(error));
-  }, []);
+  }, [newWord]);
 
   const onClick = (button) => {
     console.log('reset button pressed', button)
-    
+    setTargetWord(newWord)
   }
 
   return (
