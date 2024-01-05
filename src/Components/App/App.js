c544a6a730b15bafbdc42fb266e2e2f26717a564
Your Name <your_email@address>
2024-01-05 14:58:13 +0000

    divergent branch to test refactor of guess Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,28 +8,46 @@ const initialGuessState = ["", "", "", "", ""];
 const initialAnswerState = [];
 
 export default function App() {
-  const [totalGuessesState, setTotalGuessesState] = useState(['','','','','',''])
+  const [totalGuessesState, setTotalGuessesState] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
   const [guessState, setGuessState] = useState(initialGuessState);
+  const [currentGuessNumberState, setCurrentGuessNumberState] = useState(0);
   const [answerState, setAnswerState] = useState(initialAnswerState);
-  const [targetWord, setTargetWord] = useState('')
+  const [targetWord, setTargetWord] = useState("");
 
-  console.log('app level guessState', guessState);
-  console.log('app level answerState', answerState)
-  console.log('app level targetWord', targetWord)
+  console.log("app level guessState", guessState);
+  console.log("app level answerState", answerState);
+  console.log("app level targetWord", targetWord);
 
-  if (answerState === targetWord.toString()) return console.log("correct word guessed!")
+  if (answerState === targetWord.toString())
+    return console.log("correct word guessed!");
+
+  // keep track of which row of the available guesses we are on. Start at 1 and when a guess is made, increment
 
   return (
     <div className="App">
-      {totalGuessesState.map((guessObj, index) => <GuessesGrid guessState={guessState} answerState={answerState} />)}
-        
+      {totalGuessesState.map((guessObj, index) => (
+        <GuessesGrid
+          currentGuessNumberState={currentGuessNumberState}
+          guessState={guessState}
+          answerState={answerState}
+        />
+      ))}
       <KeyboardDisplay
         guessState={guessState}
         setGuessState={setGuessState}
         answerState={answerState}
         setAnswerState={setAnswerState}
+        currentGuessNumberState={currentGuessNumberState}
+        setCurrentGuessNumberState={setCurrentGuessNumberState}
       />
-      <ResetButton setTargetWord={setTargetWord}/>
+      <ResetButton setTargetWord={setTargetWord} />
     </div>
   );
 }
--- src/Components/Guess/Guesses.js
+++ src/Components/Guess/Guesses.js
@@ -1,19 +1,19 @@
-import Letter from "../Letter/Letter";
-import { useState } from "react";
-export default function Guesses() {
-  const initialState = ["", "", "", "", ""];
-  const [guessState, setGuessState] = useState(initialState);
+// import Letter from "../Letter/Letter";
+// import { useState } from "react";
+// export default function Guesses() {
+//   const initialState = ["", "", "", "", ""];
+//   const [guessState, setGuessState] = useState(initialState);
 
-  console.log(guessState);
-  return (
-    <section className="guesses">
-      <ul>
-        {guessState.map((item, index) => (
-          <li className="letter-card" key={index}>
-            <Letter />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+//   console.log(guessState);
+//   return (
+//     <section className="guesses">
+//       <ul>
+//         {guessState.map((item, index) => (
+//           <li className="letter-card" key={index}>
+//             <Letter />
+//           </li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// }
