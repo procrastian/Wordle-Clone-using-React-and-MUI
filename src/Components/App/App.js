4903398b5590025824bd8da76e6fc1e9597bb74b
Your Name <your_email@address>
2024-01-05 14:34:25 +0000

    compares if submitted guess matches target word

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,9 +11,13 @@ export default function App() {
   const [guessState, setGuessState] = useState(initialGuessState);
   const [answerState, setAnswerState] = useState(initialAnswerState);
   const [targetWord, setTargetWord] = useState('')
+
   console.log('app level guessState', guessState);
   console.log('app level answerState', answerState)
   console.log('app level targetWord', targetWord)
+
+  if (answerState === targetWord.toString()) return console.log("correct word guessed!")
+
   return (
     <div className="App">
       <GuessesGrid guessState={guessState} answerState={answerState} />
